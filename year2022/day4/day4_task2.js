dd30c5f1d8905a5be15fd0357bbdc0c420658cb9
Gustav Andersson <gustav87and@gmail.com>
2023-01-21 19:32:25 +0100

    Complete day 4

--- year2022/day4/day4_task2.js
+++ year2022/day4/day4_task2.js
@@ -5,17 +5,19 @@ const data = readFile(filename);
 
 const sum = data.split("\n")
   .map(pair => pair.split(","))
-  .map(pair => findCompleteOverlap(pair))
+  .map(pair => findPartialOverlap(pair))
   .reduce((a, b) => a + b, 0)
 
 console.log(sum);
 
-function findCompleteOverlap(pair) {
+function findPartialOverlap(pair) {
   // Mapping from num to +num is done to parse the string to a number. You could do num => parseInt(num), instead.
   const elf1 = pair[0].split("-").map(num => +num);
   const elf2 = pair[1].split("-").map(num => +num);
-  if (elf1[0] <= elf2[0] && elf1[1] >= elf2[1]) return 1;
-  if (elf2[0] <= elf1[0] && elf2[1] >= elf1[1]) return 1;
+  if (elf1[0] >= elf2[0] && elf1[0] <= elf2[1]) return 1;
+  if (elf1[1] <= elf2[1] && elf1[1] >= elf2[0]) return 1;
+  if (elf2[0] >= elf1[0] && elf2[0] <= elf1[1]) return 1;
+  if (elf2[1] <= elf1[1] && elf2[1] >= elf1[0]) return 1;
   return 0;
 }
 
