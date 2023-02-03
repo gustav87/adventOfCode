49e33aa935d02e5d11062e96a54293be9fa1e660
Gustav Andersson <gustav87and@gmail.com>
2023-02-03 01:16:50 +0100

    Complete day 10 task 1

---
+++ year2022/day10/day10_task1.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const filename = "day10_input.txt";
+
+const data = readFile(filename);
+
+const cycleStops = [20, 60, 100, 140, 180, 220];
+
+// We should increment cycle BEFORE we process the operation, so that signal strength is calculated correctly,
+
+let X = 1;
+let cycle = 1;
+
+const totalSignalStrength = data
+  .split("\n")
+  .map(line => {
+    let signalStrength = 0;
+    if (isCycleStop(cycle)) signalStrength = calculateSignalStrength(cycle, X);
+    cycle += 1;
+
+    if (isInstruction(line)) {
+      if (isCycleStop(cycle)) signalStrength = calculateSignalStrength(cycle, X);
+      cycle += 1;
+      const num = line.split(" ")[1];
+      X += modifyRegister(num);
+    }
+
+   return signalStrength;
+  })
+ .reduce((a, b) => a + b, 0);
+
+console.log(totalSignalStrength);
+
+function isCycleStop(cycle) {
+  return cycleStops.includes(cycle);
+}
+function calculateSignalStrength(cycle, X) {
+  return cycle * X;
+}
+function isInstruction(str) {
+  return str !== "noop";
+}
+
+function modifyRegister(i) {
+  return +i;
+}
+
+function readFile(filename) {
+  try {
+    const data = fs.readFileSync(filename, 'utf8');
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
