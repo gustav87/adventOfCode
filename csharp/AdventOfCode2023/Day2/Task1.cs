319a113a06ffff74f51c9e81db0719e58665c0a2
Gustav Andersson <gustav87and@gmail.com>
2023-12-28 16:03:08 +0100

    Complete c# day 2 task 1

---
+++ csharp/AdventOfCode2023/Day2/Task1.cs
@@ -0,0 +1,24 @@
+namespace AdventOfCode2023.Day2;
+
+using AdventOfCode2023;
+
+class Day2Task1
+{
+    public static void RunDay2Task1()
+    {
+
+        List<string> content = ReadFile1.ReadFile("Day2/day2_input.txt");
+        int sum = 0;
+        List<int> validTurns = new List<int>();
+        foreach (string line in content)
+        {
+            Game game = new();
+            game.ParseLine(line);
+            if (game.Valid)
+            {
+                sum += game.Id;
+            }
+        }
+        Console.WriteLine(sum); // 2285
+    }
+}
--- csharp/AdventOfCode2023/Program.cs
+++ csharp/AdventOfCode2023/Program.cs
@@ -1,11 +1,13 @@
 ﻿﻿namespace AdventOfCode2023;
 using AdventOfCode2023.Day1;
+using AdventOfCode2023.Day2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Day1Task1.RunDay1Task1(); // 54634
-        Day1Task2.RunDay1Task2();
+        // Day1Task1.RunDay1Task1(); // 54634
+        // Day1Task2.RunDay1Task2(); // 53855
+        Day2Task1.RunDay2Task1(); // 2285
     }
 }
